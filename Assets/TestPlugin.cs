4c41a3ce0481fcdf571549030df5d3e4ec82afd4
ninad7007 <dighe.ninad7@gmail.com>
2018-03-20 16:36:19 -0700

    First commit

---
+++ Assets/TestPlugin.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class TestPlugin : MonoBehaviour {
+    public RobotController RobotControllerobj;
+	AndroidJavaClass jc;
+    //public AudioSource jump, rage;
+    public string[] keywords = new string[] { "up", "down", "left", "right" };
+
+    // Use this for initialization
+    void Start () {  
+
+
+	}
+    void Update()
+    {
+        
+    }
+
+    public void OnButtonClick()
+	{
+		var go = EventSystem.current.currentSelectedGameObject;
+		if (go != null) {
+			Debug.Log ("Clicked on : " + go.name);
+		#if UNITY_ANDROID
+			jc = new AndroidJavaClass("com.example.speechassist.Assist");
+			jc.CallStatic("promptSpeechInput");
+		#endif
+		} else {
+			Debug.Log ("currentSelectedGameObject is null");
+		}
+	}
+
+	void onActivityResult(string Translation){
+	
+		Debug.Log (Translation);
+		GameObject.Find ("TextRecognition").GetComponent<UnityEngine.UI.Text> ().text = Translation;
+        /*int result = string.CompareOrdinal(Translation,"jump");
+        if (result == 0)
+        {
+            jump.Play();
+        }
+        result = string.CompareOrdinal(Translation, "rage");
+        if (result == 0)
+        {
+            rage.Play();
+        }*/
+        RobotControllerobj.RobotActions(Translation);
+    }
+}
---
+++ Assets1/myScript/RobotController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotController : MonoBehaviour {
+    AudioSource run, attack, go_crazy, walk;
+    public Animator anim;
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim = GetComponent<Animator>();
+        run = GameObject.Find("run").GetComponent<AudioSource>();
+        attack = GameObject.Find("attack").GetComponent<AudioSource>();
+        go_crazy = GameObject.Find("gocrazy").GetComponent<AudioSource>();
+        walk = GameObject.Find("walk").GetComponent<AudioSource>();
+    }
+        public void RobotActions(string Actions)
+    {
+        int result = string.CompareOrdinal(Actions, "run");
+        if (result == 0)
+        {
+            anim.Play("Run_IP", -1, 0f);
+            run.Play();
+        }
+        result = string.CompareOrdinal(Actions, "attack");
+        if (result == 0)
+        {
+            anim.Play("Attack_Arm_1", -1, 0f);
+            attack.Play();
+        }
+        result = string.CompareOrdinal(Actions, "go crazy");
+        if (result == 0)
+        {
+            anim.Play("Idle_Crazy_Robot", -1, 0f);
+            go_crazy.Play();
+        }
+        result = string.CompareOrdinal(Actions, "walk");
+        if (result == 0)
+        {
+            anim.Play("Walk_IP", -1, 0f);
+            walk.Play();
+        }
+    }
+}
+    
+
